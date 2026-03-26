adc7e6a671dfaec54fd955a13e436cf279697c2f
Graceman <adetoyesamuel63@gmail.com>
2026-03-26 06:35:42 -0700

    base jpa fixed

--- src/main/java/com/lamiplus_common_api/api/BasePluginJpaConfig.java
+++ src/main/java/com/lamiplus_common_api/api/BasePluginJpaConfig.java
@@ -1,7 +1,7 @@
 package com.lamiplus_common_api.api;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -9,14 +9,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import jakarta.persistence.EntityManagerFactory;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+
 @EnableTransactionManagement
 public abstract class BasePluginJpaConfig {
 
     @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    public PlatformTransactionManager transactionManager(
+            // Explicitly qualify to the plugin-local EMF singleton we registered.
+            // Without @Qualifier, Spring might walk up to the parent context and
+            // find the core application's entityManagerFactory instead.
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
+
+
     }
 }
