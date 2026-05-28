1149d56fefc31123c24371b3b2ec0036b402abc4
Chukwuemeka <ilozue.chukwuemeka@thepalladiumgroup.com>
2026-05-28 10:16:04 +0100

    feat: replace BaseAudit dependency with audit annotation

---
+++ src/main/java/com/lamiplus_common_api/audit/AuditUpdatedDate.java
@@ -0,0 +1,13 @@
+package com.lamiplus_common_api.audit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuditUpdatedDate {
+    boolean required() default false;
+    boolean updatable() default true;
+}
